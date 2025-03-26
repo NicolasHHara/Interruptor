f8f72b35b529fbfb1eb092d8d4a9b96e60b6e824
Nicolas <nico.hhara@gmail.com>
2025-03-25 21:36:20 -0300

    sdds

---
+++ script.js
@@ -0,0 +1,10 @@
+let btnOn = document.getElementById ("On")
+let btnOff = document.getElementById ("Off")
+
+btnOff.addEventListener ("click", () => {
+     
+})
+
+btnOn.addEventListener ("click", () => {
+
+})
